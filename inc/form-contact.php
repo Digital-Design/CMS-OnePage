72d17dd7f34e4d61e56bc73418c38b479ccaba7c
Konstantin Sidorenko <konstantin@yellowcake.net>
2015-11-20 11:36:31 +0100

    revoir mail

--- inc/form-contact.php
+++ inc/form-contact.php
@@ -17,7 +17,7 @@ $stmt->bindParam(':nom', $_POST['nom'], PDO::PARAM_STR);
 $stmt->bindParam(':commentaire', $_POST['commentaire'], PDO::PARAM_STR);
 
 if($stmt->execute() or die(var_dump($stmt->ErrorInfo()))) {
-
+/*
   // Retrieve the email template required
   $message = file_get_contents('../'.ADMIN_FOLDER.'/vues/sample_mail.html');
 
@@ -51,7 +51,25 @@ if($stmt->execute() or die(var_dump($stmt->ErrorInfo()))) {
     echo "invalid";
     //echo 'Mailer Error: ' . $mail->ErrorInfo;
   }
+*/
+    // Le message
+  //$message = $_POST['commentaire'];
 
+  // Dans le cas où nos lignes comportent plus de 70 caractères, nous les coupons en utilisant wordwrap()
+  //$message = wordwrap($message, 70, "\r\n");
+  $message = file_get_contents('../'.ADMIN_FOLDER.'/vues/sample_mail.html');
+
+  $message = str_replace('%mail%', $_POST['mail'], $message);
+  $message = str_replace('%nom%', $_POST['nom'], $message);
+  $message = str_replace('%commentaire%', $_POST['commentaire'], $message);
+  $message = str_replace('%titre%', 'Vous avez été contacté depuis votre site web', $message);
+  $message = str_replace('%domaine%', $_SERVER['HTTP_HOST'], $message);
+
+  $headers = 'From: Anniversaire <anniversaire@example.com>' . "\r\n";
+
+  // Envoi du mail
+  if(mail(ADMIN_ADRESSE, 'Vous avez été contacté depuis votre site web', $message, $headers)) echo "success";
+  else echo "invalid";
 }else{
   echo "invalid";
 }
