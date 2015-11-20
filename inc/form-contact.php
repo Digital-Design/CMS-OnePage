90dda7ce38841f6b8b4cd2505fc8260979f57dde
Konstantin Sidorenko <konstantin@yellowcake.net>
2015-11-20 14:17:59 +0100

    fix mail

--- inc/form-contact.php
+++ inc/form-contact.php
@@ -1,75 +1,83 @@
 <?php
-require('config.inc.php');
-require('../'.ADMIN_FOLDER.'/modeles/index.php');
-require('../'.ADMIN_FOLDER.'/modeles/mailer.php');
 
-$bdd = connectDB();
+if(!empty($_POST)) {
 
-$sql = 'INSERT INTO contact SET
-nom = :nom,
-mail = :mail,
-commentaire = :commentaire';
+  require('config.inc.php');
+  require('../'.ADMIN_FOLDER.'/modeles/index.php');
+  require('../'.ADMIN_FOLDER.'/modeles/mailer.php');
+  require('../'.ADMIN_FOLDER.'/modeles/parametre.php');
 
-$stmt = $bdd->prepare($sql);
+  $bdd = connectDB();
 
-$stmt->bindParam(':mail', $_POST['mail'], PDO::PARAM_STR);
-$stmt->bindParam(':nom', $_POST['nom'], PDO::PARAM_STR);
-$stmt->bindParam(':commentaire', $_POST['commentaire'], PDO::PARAM_STR);
+  $sql = 'INSERT INTO contact SET
+  nom = :nom,
+  mail = :mail,
+  commentaire = :commentaire';
 
-if($stmt->execute() or die(var_dump($stmt->ErrorInfo()))) {
+  $stmt = $bdd->prepare($sql);
+
+  $stmt->bindParam(':mail', $_POST['mail'], PDO::PARAM_STR);
+  $stmt->bindParam(':nom', $_POST['nom'], PDO::PARAM_STR);
+  $stmt->bindParam(':commentaire', $_POST['commentaire'], PDO::PARAM_STR);
+
+  if($stmt->execute() or die(var_dump($stmt->ErrorInfo()))) {
+
+    // Retrieve the email template required
+    $message = file_get_contents('../'.ADMIN_FOLDER.'/vues/sample_mail.html');
+
+    // Replace the % with the actual information
+    $message = str_replace('%mail%', $_POST['mail'], $message);
+    $message = str_replace('%nom%', $_POST['nom'], $message);
+    $message = str_replace('%commentaire%', $_POST['commentaire'], $message);
+    $message = str_replace('%titre%', 'Vous avez été contacté depuis votre site web', $message);
+    $message = str_replace('%domaine%', $_SERVER['HTTP_HOST'], $message);
+
+    // Setup PHPMailer
+    $mail = new PHPMailer();
+
+    // Set who the email is coming from
+    $mail->SetFrom($_POST['mail'], $_POST['nom']);
+
+    // Set who the email is sending to
+    $mail->AddAddress(ADMIN_ADRESSE);
+
+    // Set the subject
+    $mail->Subject = 'Vous avez été contacté depuis votre site web';
+
+    //Set the message
+    $mail->MsgHTML($message);
+    $mail->AltBody = strip_tags($message);
+
+    // Send the email
+    if($mail->Send()) {
+      echo "success";
+    }else{
+      echo "invalid";
+      //echo 'Mailer Error: ' . $mail->ErrorInfo;
+    }
+
 /*
-  // Retrieve the email template required
-  $message = file_get_contents('../'.ADMIN_FOLDER.'/vues/sample_mail.html');
+    //recuperation du mail admin
+    $parametre = getParametre(3);
 
-  // Replace the % with the actual information
-  $message = str_replace('%mail%', $_POST['mail'], $message);
-  $message = str_replace('%nom%', $_POST['nom'], $message);
-  $message = str_replace('%commentaire%', $_POST['commentaire'], $message);
-  $message = str_replace('%titre%', 'Vous avez été contacté depuis votre site web', $message);
-  $message = str_replace('%domaine%', $_SERVER['HTTP_HOST'], $message);
+    //recuperatoin du template
+    $message = file_get_contents('../'.ADMIN_FOLDER.'/vues/sample_mail.html');
 
-  // Setup PHPMailer
-  $mail = new PHPMailer();
+    $message = str_replace('%mail%', $_POST['mail'], $message);
+    $message = str_replace('%nom%', $_POST['nom'], $message);
+    $message = str_replace('%commentaire%', $_POST['commentaire'], $message);
+    $message = str_replace('%titre%', 'Vous avez été contacté depuis votre site web', $message);
+    $message = str_replace('%domaine%', $_SERVER['HTTP_HOST'], $message);
 
-  // Set who the email is coming from
-  $mail->SetFrom($_POST['mail'], $_POST['nom']);
+    $headers = 'From: Votre CMS <notification@cmsonepage.fr>' . "\r\n";
+    $headers .= "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-  // Set who the email is sending to
-  $mail->AddAddress(ADMIN_ADRESSE);
-
-  // Set the subject
-  $mail->Subject = 'Vous avez été contacté depuis votre site web';
-
-  //Set the message
-  $mail->MsgHTML($message);
-  $mail->AltBody = strip_tags($message);
-
-  // Send the email
-  if($mail->Send()) {
-    echo "success";
+    // Envoi du mail
+    if(mail($parametre['valeur'], 'Vous avez été contacté depuis votre site web', $message, $headers)) echo "success";
+    else echo "invalid";
+*/
   }else{
     echo "invalid";
-    //echo 'Mailer Error: ' . $mail->ErrorInfo;
   }
-*/
-    // Le message
-  //$message = $_POST['commentaire'];
-
-  // Dans le cas où nos lignes comportent plus de 70 caractères, nous les coupons en utilisant wordwrap()
-  //$message = wordwrap($message, 70, "\r\n");
-  $message = file_get_contents('../'.ADMIN_FOLDER.'/vues/sample_mail.html');
-
-  $message = str_replace('%mail%', $_POST['mail'], $message);
-  $message = str_replace('%nom%', $_POST['nom'], $message);
-  $message = str_replace('%commentaire%', $_POST['commentaire'], $message);
-  $message = str_replace('%titre%', 'Vous avez été contacté depuis votre site web', $message);
-  $message = str_replace('%domaine%', $_SERVER['HTTP_HOST'], $message);
-
-  $headers = 'From: Anniversaire <anniversaire@example.com>' . "\r\n";
-
-  // Envoi du mail
-  if(mail(ADMIN_ADRESSE, 'Vous avez été contacté depuis votre site web', $message, $headers)) echo "success";
-  else echo "invalid";
-}else{
-  echo "invalid";
 }
